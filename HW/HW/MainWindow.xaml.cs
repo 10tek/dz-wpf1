31966feea786b859dd9e9705fab41a2ec5237d9a
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-11-24 04:19:00 +0600

    Браузер с вкладками

---
+++ HW/HW/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchBtnClick(object sender, RoutedEventArgs e)
+        {
+            browserWB.Navigate(new Uri(urlTB.Text, UriKind.RelativeOrAbsolute));
+        }
+
+        private void AddBtnClick(object sender, RoutedEventArgs e)
+        {
+            //Пытался сделать независимую вкладку, типо 2разных сайта, не смог склонировать объект.
+            //как в структуре obj1 = obj2 и они 2 разных объекта.
+            tabControl.Items.Add(new TabItem
+            {
+                Header = new TextBlock { Text = "Вкладка" },
+                Content = tabSP
+            });
+        }
+    }
+}
